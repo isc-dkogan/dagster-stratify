03fba215a7645e88d4e9808204b28b4242810875
David Kogan <dkogan@usmbp16dkogan2.home>
2024-01-31 23:23:31 -0500

    histogram in local file, using ops

--- dagster_stratify/__init__.py
+++ dagster_stratify/__init__.py
@@ -1,38 +1,23 @@
-from dagster import Definitions, load_assets_from_modules, AssetSelection, define_asset_job, ScheduleDefinition
+from dagster import job, schedule, RunRequest, Definitions
+from . import ops
 
-from . import assets
+@job
+def config_validation_job():
+    ops.config_validation_op()
 
-all_assets = load_assets_from_modules([assets])
+@job
+def computations_job():
+    ops.computations_op()
 
+@schedule(job=config_validation_job, cron_schedule="15 18 * * 1-5")
+def config_validation_schedule():
+    return RunRequest()
+
+@schedule(job=computations_job, cron_schedule="6 15 * * 1-5")
+def computations_schedule():
+    return RunRequest()
+    
 defs = Definitions(
-    assets=all_assets,
-)
-
-config_validation = AssetSelection.keys("config_validation")
-computations = AssetSelection.keys("computations")
-
-config_validation_job = define_asset_job(
-  name="config_validation_job",
-  selection=config_validation,
-)
-
-computations_job = define_asset_job(
-  name="computations_job",
-  selection=computations,
-)
-
-config_validation_schedule = ScheduleDefinition(
-    job=config_validation_job,
-    cron_schedule="55 20 * * 1-5",
-)
-
-computations_schedule = ScheduleDefinition(
-    job=computations_job,
-    cron_schedule="6 15 * * 1-5",
-)
-
-defs = Definitions(
-    assets=all_assets,
     jobs=[config_validation_job, computations_job],
-    schedules=[config_validation_schedule, computations_schedule]
+    schedules=[config_validation_schedule, computations_schedule],
 )
